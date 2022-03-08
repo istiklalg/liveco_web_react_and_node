88818238dc646ef1b5158689068e5f2ded41335d
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 17:50:04 +0300

    Update AddOrUpdateProduct.js

--- CLIENTSIDE/src/components/products/AddOrUpdateProduct.js
+++ CLIENTSIDE/src/components/products/AddOrUpdateProduct.js
@@ -1,7 +1,4 @@
 /* @author : istiklal */
-/* add and update forms for products */
-// useEffect componentDidMount yerine
-//useState ise set state yerine kullanacağımız fonksiyonlar olacak
 
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
@@ -18,15 +15,13 @@ alertify.set("notifier", "position", "bottom-left");
 function AddOrUpdateProduct({
   products,
   categories,
-  // loadingImage,
   getProducts,
   getCategories,
   saveProduct,
   history,
-  ...props // burada mevcut propsları bu şekilde genişletebiliyoruz. Yani bu fonksiyonun propları ve yukarıdakiler demek oluyor.
+  ...props
 }) {
-  const [product, setProduct] = useState({ ...props.product }); // bu kullanıma javascript te distructing denir.
-  // bu syntax state deki product ı setProduct ile set edebilirim demek oluyor react-hook dan yararlanıyoruz.
+  const [product, setProduct] = useState({ ...props.product }); 
   const token = getToken();
   const [errors, setErrors] = useState({});
 
@@ -36,12 +31,11 @@ function AddOrUpdateProduct({
 
   useEffect(() => {
     if (categories.length === 0) {
-      // bu demek oluyorki kategoriler sayfasına hiç gidilmeden bu sayfaya gelinmiş !
-      getCategories(); // bunu yaparak categories listesini dolduruyoruz.
+
+      getCategories(); 
     }
     setProduct({ ...props.product });
   }, [props.product, categories, getCategories]);
-  // [props.product] vermek useEffect in sonsuz döngüye girmesini engellemektedir, bunun anlamı props.product ı izle bu doma yerleştiği zaman process i bitir demek
 
   function handleChange(event) {
     const { name, value, files } = event.target;
@@ -51,8 +45,8 @@ function AddOrUpdateProduct({
     setProduct((previousProduct) => ({
       ...previousProduct,
       [name]: name === "productImage" && files ? files[0].name : value,
-      // [name]: name === "productImage" ? ( files && files[0] ? files[0].name : "resim hatalı") : value,
-      [name]: name === "categoryId" ? parseInt(value, 10) : value, // önceki product ın name i o olan alanındeğerini atıyorız categoryId olan alanı integer a çeviriyoruz
+
+      [name]: name === "categoryId" ? parseInt(value, 10) : value,
     }));
     validate(name, value);
   }
