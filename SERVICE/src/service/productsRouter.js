71de51b385a6f7973bede9092ababa42e035c872
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 17:28:11 +0300

    Update productsRouter.js

--- SERVICE/src/service/productsRouter.js
+++ SERVICE/src/service/productsRouter.js
@@ -4,18 +4,17 @@ const db = require("./db");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+/** @author: istiklal */
+
 const resultList = [];
 
 router.use(bodyParser.json());
-// router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
   console.log("running for [GET].../products");
-  // console.log("request : ", req);
+
   console.log("Query : ", req.query);
   if (req.query.id) {
-    // && Number.isInteger(req.query.id)
-    // req.query.id.includes("\"")
     db.db.get(`SELECT * FROM products WHERE id=${req.query.id}`, (err, row) => {
       if (err) {
         console.error(err.message);
@@ -56,8 +55,6 @@ router.post("/", (req, res, next) => {
   console.log("post request body : ", req.body);
   let newProduct = req.body ? req.body : {}
   if(newProduct && !newProduct.id){
-    //INSERT INTO products (productName, categoryId, quantityPerUnit, unitPrice, unitsInStock, productImage)
-    //VALUES ("Domates Salçası", 4, "250 gramlık kavanoz", 125.70, 15, "domates salçası.jpg")
     let stmt = `INSERT INTO products (productName, categoryId, quantityPerUnit, unitPrice, unitsInStock, productImage)
     VALUES ("${newProduct.productName}", ${newProduct.categoryId}, "${newProduct.quantityPerUnit}",
      ${newProduct.unitPrice}, ${newProduct.unitsInStock}, "${newProduct.productImage}")`
@@ -70,14 +67,10 @@ router.post("/", (req, res, next) => {
       }
     })
   }
-  // res.status(200).json(req.body);
-  // products.push({name: req.body.name, price: req.body.price, image: req.body.image, description: req.body.description});
-  // res.redirect("/");
 });
 
 router.put("/:id", (req, res, next) => {
   console.log("running for [PUT].../products");
-  // console.log("request : ", req);
   console.log("put request body : ", req.body);
   let updatedProduct = req.body ? req.body : {}
   if(updatedProduct && updatedProduct.id){
@@ -93,10 +86,7 @@ router.put("/:id", (req, res, next) => {
       }
     })
   }
-
-  // res.status(200).json({ok: true});
 })
 
-// module.exports = router;
 exports.router = router;
 exports.resultList = resultList;
