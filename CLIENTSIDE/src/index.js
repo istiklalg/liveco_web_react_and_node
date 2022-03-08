0dc1479e3c749c242965478cb597ac3d7fd1ab91
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 17:33:17 +0300

    Update index.js

--- CLIENTSIDE/src/index.js
+++ CLIENTSIDE/src/index.js
@@ -8,13 +8,8 @@ import {Provider} from "react-redux";
 import configureStore from "./redux/reducers/configureStore";
 import {BrowserRouter} from "react-router-dom";
 
+/** @author: istiklal */
+
 const store = configureStore();
 
 ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>,document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// serviceWorker.unregister();
-// reportWebVitals();
